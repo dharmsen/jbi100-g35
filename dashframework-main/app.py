a9125909855bf4fcaff8c12475ec498185e065a5
Egor-Watson <watson4cccp@gmail.com>
2022-01-25 19:01:50 +0400

    Improved map vis.

--- dashframework-main/app.py
+++ dashframework-main/app.py
@@ -10,6 +10,7 @@ from jbi100_app.views.layout import generate_nav_bar, generate_basic_layout, gen
 from dash import html, dcc, dash_table
 import plotly.express as px
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 import pandas as pd
 from datetime import date
 
@@ -45,7 +46,17 @@ table = html.Div(
 df_map = df_location.join(df_severity, rsuffix='_b')
 df_map = df_map.join(df_date, rsuffix='_c')
 
+
 print(df_map.columns)
+
+# Computes data points per year, used for ensuring not too many data points on map
+
+years = list(df_map.drop_duplicates(subset=['accident_year'])['accident_year'])
+yearCount = {}
+for year in years:
+    count = df_map[df_map['accident_year'] == year]['accident_year'].count()
+    yearCount[year] = count
+
 
 # 500,000 nominal performance (probably best to stay here to ensure resources are left for other visualizations)
 # 750,000 not great performance
@@ -81,6 +92,142 @@ app.layout = html.Div(
 )
 
 
+def compute_size(startYear, endYear):
+    total = 0
+    years = list(range(startYear, endYear + 1))
+    print(years)
+    for year in years:
+        total += yearCount[year]
+
+    return total
+
+
+def check_size_old(startYear, endYear):
+    print('Running check_size_old')
+    # total = 0
+    # years = list(range(startYear, endYear+1))
+    # print(years)
+    # for year in years:
+    #     total += yearCount[year]
+    years = list(range(startYear, endYear + 1))
+    total = compute_size(startYear, endYear)
+
+    print("Total at start: " + str(total))
+    # If more than 500k reduce by 1 year and check again
+    if total > 500000:
+        # generate flipping pattern to cut list from front and back
+        # cut from 0, -1, 1, -2, 2, -3, 3
+        flipping = []
+        for i in range(0, len(years)):
+            if i == 0:
+                flipping.append(i)
+            else:
+                flipping.append(i*-1)
+                flipping.append(i)
+
+        print(flipping)
+        toRemove = []
+        for i in flipping:
+            if total > 500000:
+                print("Removed: " + str(years[i]))
+                total -= yearCount[years[i]]
+                toRemove.append(years[i])
+            else:
+                break
+
+        # drop excess years
+        for year in toRemove:
+            years.remove(year)
+
+        new_range = [years[0], years[1], total]
+        print("Left with: " + str(total))
+        print(new_range)
+        return new_range
+
+    print("Left with: " + str(total))
+    print([startYear, endYear])
+    return [startYear, endYear, total]
+
+
+@app.callback(
+    Output('map-info', 'children'),
+    Output('map-info', 'style'),
+    Input('map-range-slider', 'value')
+)
+def slider(value):
+    size = compute_size(value[0], value[1])
+    print("GOt: " + str(size))
+    error_style_font = {'color': 'red'}
+
+    if size > 500000:
+        return 'Data points loaded: EXCEEDED', error_style_font
+    else:
+        return 'Data points loaded: %s' % size, {}
+    # new_values = check_size_old(value[0], value[1])
+    # message = 'Data points loaded: %s' % new_values[2]
+    #
+    # slider_values = [new_values[0], new_values[1]]
+    # return message, slider_values
+    # # print("fired slider callback")
+    # # return 'wow'
+
+
+# Does actual map computing
+@app.callback(
+    Output('map', 'figure'),
+    Input('map-info', 'children'),
+    Input('map-range-slider', 'value'),
+    Input('color-dropdown', 'value'),
+    Input('size-dropdown', 'value'),
+    # Input("loading-input-1-1", "value")
+)
+# Output("loading-output-1", "children"), Input("loading-input-1", "value")
+def do_map(txt, range, color_drop, size_drop):#, value):
+
+    if txt != 'Data points loaded: EXCEEDED':
+        df_map_filtered = df_map[(df_map['accident_year'] >= range[0]) &
+                                 (df_map['accident_year'] <= range[1])]
+        print('Updating map!')
+        return m.create_figure(df_map_filtered, color_drop, size_drop)
+    else:
+        # prevent update
+        raise PreventUpdate
+
+
+@app.callback(
+    Output('map-ok-button', 'children'),
+    Input('map', 'figure')
+)
+def testy(fig):
+    return fig.is_loading
+
+@app.callback(
+    Output('map-hidden-panel', 'style'),
+    Output('color-dropdown', 'options'),
+    Output('size-dropdown', 'options'),
+    Input('options-button', 'n_clicks')
+)
+def openOptions(value):
+
+    # Get channel options for map
+    columns = [
+            {'label': 'Police Force', 'value': 'police_force'},
+            {'label': 'Accident Severity', 'value': 'accident_severity'},
+            {'label': 'Number of Vehicles', 'value': 'number_of_vehicles'},
+            {'label': 'Deaths', 'value': 'number_of_casualties'},
+            {'label': 'Day of Week', 'value': 'day_of_week'},
+        ]
+
+
+    # Makes pop up visible/ hidden
+    if value is not None:
+        if value % 2 == 1:
+            return {'visibility': 'visible', 'opacity': '1'}, columns, columns
+
+    return {'visibility' : 'hidden', 'opacity': '0'}, columns, columns
+
+
+
 # Global filter callback function
 @app.callback(
     Output('table', 'columns'), # TODO: only changing current table, will need to change to other vis' i think
@@ -103,33 +250,27 @@ def global_filter(year_range, time_range, vehicle_no, start_date, end_date):
     # return '{}, {}, {}, {}, {}'.format(year_range, time_range, vehicle_no, start_date, end_date)
     return columns, data
 
-# Create visualizations
-# Simple bar chart
-@app.callback(
-    Output('loading-output-1', 'children'), # Loading indicator
-    Output(simple_barchart.html_id, 'figure'),
-    Input('year-filter-global', 'value') # dummy input, else callback doesn't work
-)
-def update_simple_barchart(value):
-    return 'Barchart loaded', simple_barchart.update()
-
-# Map callbacks
-@app.callback(
-    Output('map', 'figure'),
-    Input('map-range-slider', 'value')
-)
-def update_map(value):
-    # Filter copy of df_map for given dates
-    print('Year 1: ' + str(value[0]))
-    print('Year 2: ' + str(value[1]))
-    df_map_filtered = df_map[(df_map['accident_year'] >= value[0]) & (df_map['accident_year'] <= value[1])]
-
-    # TODO: figure out how to only render 500k rows max, maybe save number of rows for each year and
-    #  # TODO: then quick dictionary look up to make sure total is less than 500k, else just set year to next one up avoiding exceeding 500k
-
-    # TODO: add loading icon
-    return m.create_figure(df_map_filtered)
 
 
 if __name__ == '__main__':
     app.run_server(debug=False, host='0.0.0.0')  # why debug not working :(
+
+# @app.callback(
+#     Output('map', 'figure'),
+#     Output('map-range-slider', 'value'),
+#     Input('map-range-slider', 'value'),
+#     Input('color-dropdown', 'value'),
+#     Input('size-dropdown', 'value'),
+# )
+# def update_map(value, color_drop, size_drop):
+#     print(color_drop)
+#     print(size_drop)
+#     # Filter copy of df_map for given dates
+#     print('Year 1: ' + str(value[0]))
+#     print('Year 2: ' + str(value[1]))
+#
+#     new_values = check_size(value[0], value[1])
+#
+#     df_map_filtered = df_map[(df_map['accident_year'] >= new_values[0]) & (df_map['accident_year'] <= new_values[1])]
+#
+#     return m.create_figure(df_map_filtered, color_drop, size_drop), new_values
--- dashframework-main/jbi100_app/visualizations/map.py
+++ dashframework-main/jbi100_app/visualizations/map.py
@@ -22,13 +22,29 @@ class Map_Visualization():
         # geojson = Map_Helper(data)
 
         # create figure
-        self.fig = self.create_figure(data)
+        self.fig = self.create_figure(data, 'number_of_vehicles', 'number_of_casualties')
         self.range_filter_global_settings = range_filter_global_settings
 
 
-    def create_figure(self, data):
-        fig = px.scatter_mapbox(data, lat="latitude", lon="longitude", color='number_of_vehicles',
-                                size='number_of_casualties',
+    def create_figure(self, data, color, size):
+
+        fig = px.scatter_mapbox(data, lat="latitude", lon="longitude", color=color,
+                                size=size,
+                                hover_data= {size: True,
+                                             color: True,
+                                             'time': True,
+                                             'date': True
+                                             },
+                                labels={'police_force': 'Police Force',
+                                        'accident_severity': ' Accident Severity',
+                                        'number_of_vehicles': 'Number Of Vehicles',
+                                        'number_of_casualties': 'Deaths',
+                                        'day_of_week': 'Day Of Week',
+                                        'Latitude': 'latitude',
+                                        'Longitude': 'longitude',
+                                        'Time': 'time',
+                                        'Date': 'date',
+                                        },
                                 color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=2)
 
         # update fig
@@ -51,25 +67,199 @@ class Map_Visualization():
         return html.Div(
             id='map-container',
             children=[
-                dcc.Graph(id='map', style={'height': '100%'}, figure=self.fig),
-                dcc.RangeSlider(
-                    id='map-range-slider',
-                    min=self.range_filter_global_settings['minYear'],
-                    max=self.range_filter_global_settings['maxYear'],
-                    step=2,
+                # dummy input to have loading screen
+                # dcc.Input(id="loading-input-1-1",
+                #           value='Input triggers local spinner',
+                #           style={'visibility': 'hidden',
+                #                  'position': 'absolute'}),
+                # dcc.Loading(
+                #     id="loading-1-1",
+                #     type="default",
+                #     style={'height': '100%'},
+                #     children=dcc.Graph(id='map', style={'height': '100%'},
+                #                        figure=self.fig),
+                # ),
+                dcc.Graph(id='map', style={'height': '100%'},
+                          figure=self.fig),
 
-                    # TODO: figure out how to only have a max distance of 3 between both handles
-                    marks={
-                        str(self.range_filter_global_settings['minYear']): {'label':
-                                                                           self.range_filter_global_settings[
-                                                                               'minYear'],
-                                                                       'style': {'color': '#fff'}},
-                        str(self.range_filter_global_settings['maxYear']): {'label':
-                                                                           self.range_filter_global_settings[
-                                                                               'maxYear'],
-                                                                       'style': {'color': '#fff'}},
-                    },
-                    value=[2019, 2020]
+
+                # Define controls for map vis in this div
+                html.Div(
+                    id='map-control-panel',
+                    children=[
+                        dcc.RangeSlider(
+                            id='map-range-slider',
+                            min=self.range_filter_global_settings['minYear'],
+                            max=self.range_filter_global_settings['maxYear'],
+                            step=1,
+                            tooltip={"placement": "bottom", "always_visible": True},
+
+                            # TODO: figure out how to only have a max distance of 3 between both handles
+                            marks={
+                                str(self.range_filter_global_settings['minYear']): {'label':
+                                                                                        self.range_filter_global_settings[
+                                                                                            'minYear'],
+                                                                                    'style': {'color': '#fff'}},
+                                str(self.range_filter_global_settings['maxYear']): {'label':
+                                                                                        self.range_filter_global_settings[
+                                                                                            'maxYear'],
+                                                                                    'style': {'color': '#fff'}},
+                            },
+                            value=[2019, 2020],
+                        ),
+                        html.Button(
+                            'Ok',
+                            id='map-ok-button',
+                            className='btn btn-primary',
+                        ),
+                        html.Div(
+                          id='map-tool-tip',
+                          children=['Click this button to confirm '
+                                    'selection and filter map.']
+                        ),
+                        # Options drop down
+                        html.Div(
+
+                            id='map-options',
+                            children=[
+                                # <input class="btn btn-primary" type="button" value="Input">
+                                # dcc.Input(
+                                #     id='options-button',
+                                #     className='btn btn-primary',
+                                #     type='button',
+                                #     value='OpenOptions'
+                                # ),
+                                html.Button(
+                                    id='options-button',
+                                    className='btn btn-primary',
+                                    children=[
+                                        html.Div(
+                                            className='button-wrapper',
+                                            children=[
+                                                html.P('Options'),
+                                                html.Img(
+                                                    src="/assets/bootstrap-icons-1.7.2/caret-down-square.svg",
+                                                    alt="Drop down icon",
+                                                    width="24",
+                                                    height="24"
+                                                ),
+                                            ]
+                                        )
+
+                                    ],
+                                ),
+                                html.Div(
+                                    id='map-hidden-panel',
+                                    children=[
+                                        html.P('Choose which channels represent what data'),
+                                        html.Div(
+                                            className='drop-down-label',
+                                            children=[
+                                                html.P('Color: '),
+                                                dcc.Dropdown(
+                                                    id='color-dropdown',
+                                                    options=[
+                                                        # # TODO: fill with options from df
+                                                        # {'label': 'New York City', 'value': 'NYC'},
+                                                        # {'label': 'Montreal', 'value': 'MTL'},
+                                                        # {'label': 'San Francisco', 'value': 'SF'}
+                                                    ],
+                                                    value='number_of_vehicles',
+                                                    style={'width': '100%'}
+                                                )
+                                            ]
+                                        ),
+                                        html.Div(
+                                            className='drop-down-label',
+                                            children=[
+                                                html.P('Size: '),
+                                                dcc.Dropdown(
+                                                    id='size-dropdown',
+                                                    options=[
+                                                        # # TODO: fill with options from df
+                                                        # {'label': 'New York City', 'value': 'NYC'},
+                                                        # {'label': 'Montreal', 'value': 'MTL'},
+                                                        # {'label': 'San Francisco', 'value': 'SF'}
+                                                    ],
+                                                    # default value
+                                                    value='number_of_casualties',
+                                                    style={'width': '100%'}
+                                                )
+                                            ]
+                                        ),
+                                    ]
+                                )
+                                # Hack using check box instead of button to open hidden menu
+                                # dcc.Input(
+                                #     id='btnControl',
+                                #     type='checkbox'
+                                # ),
+                                # html.Label(
+                                #     id='btn',
+                                #     htmlFor='btnControl',
+                                #     children=[
+                                #         html.P('Options'),
+                                #         html.Img(
+                                #             src="/assets/bootstrap-icons-1.7.2/caret-down-square.svg",
+                                #             alt="Drop down icon",
+                                #             width="24",
+                                #             height="24"
+                                #         ),
+                                #         html.Div(
+                                #             id='map-hidden-panel',
+                                #             children=[
+                                #                 html.P('Choose which channels represent what data'),
+                                #                 html.Span(
+                                #                     className='drop-down-label',
+                                #                     children=[
+                                #                         html.P('Color: '),
+                                #                         dcc.Dropdown(
+                                #                             id='color-dropdown',
+                                #                             options=[
+                                #                                 # TODO: fill with options from df
+                                #                                 {'label': 'New York City', 'value': 'NYC'},
+                                #                                 {'label': 'Montreal', 'value': 'MTL'},
+                                #                                 {'label': 'San Francisco', 'value': 'SF'}
+                                #                             ],
+                                #                             value='NYC'
+                                #                         )
+                                #                     ]
+                                #                 ),
+                                #                 html.Span(
+                                #                     className='drop-down-label',
+                                #                     children=[
+                                #                         html.P('Size: '),
+                                #                         dcc.Dropdown(
+                                #                             id='size-dropdown',
+                                #                             options=[
+                                #                                 # TODO: fill with options from df
+                                #                                 {'label': 'New York City', 'value': 'NYC'},
+                                #                                 {'label': 'Montreal', 'value': 'MTL'},
+                                #                                 {'label': 'San Francisco', 'value': 'SF'}
+                                #                             ],
+                                #                             value='NYC'
+                                #                         )
+                                #                     ]
+                                #                 ),
+                                #             ]
+                                #         )
+                                #     ]
+                                # ),
+
+
+                            ]
+                        ),
+                        # rows indicator
+                        html.Div(
+                            id='map-info-panel',
+                            children=[
+                                # displays: Data points loaded: XXX
+                                html.P(
+                                    id='map-info',
+                                    children=['Data points loaded: '])
+                            ]
+                        ),
+                    ]
                 )
             ]
         )
